46a0fe35f30ae9c8f6ca14ea4d70f2d15dc11a66
danieljreid <61446527+danieljreid@users.noreply.github.com>
2023-11-19 22:10:52 +1100

    Update create_profile.js

--- create_profile.js
+++ create_profile.js
@@ -4,13 +4,12 @@ document.getElementById('profile-form').addEventListener('submit', function(even
     // Retrieve form values
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
     var location = document.getElementById('location').value;
 
-    // You can send this data to a server or store it locally, depending on your needs
-    // For now, let's just log the values
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Location:', location);
+    // Store the data in localStorage
+    var profileData = { name, email, phone, location };
+    localStorage.setItem('userProfile', JSON.stringify(profileData));
 
     // Redirect the user to the profile page
     window.location.href = 'profile.html';
